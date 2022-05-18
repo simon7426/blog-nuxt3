cdd9bc8d211e13c4f116640d185390cf2649b916
simon.islam <simon.islam@brilliant.com.bd>
2022-05-18 16:06:52 +0600

    changed blog card

--- components/AppPostCard.vue
+++ components/AppPostCard.vue
@@ -1,30 +1,42 @@
 <template>
-  <article>
+  <div
+    class="p-6 bg-white rounded-lb border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+  >
+    <a href="#">
+      <h5
+        class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+      >
+        {{ post.title }}
+      </h5>
+    </a>
     <dl>
       <dt class="sr-only">Published on</dt>
       <dd class="font-medium text-gray-500">
         <time> {{ $moment(post.created_at).format('MMMM DD, YYYYY') }} </time>
       </dd>
     </dl>
-    <div class="space-y-5">
-      <NuxtLink to="#">
-        <h2 class="text-3xl font-bold tracking-tight text-gray-900">
-          {{ post.title }}
-        </h2>
-      </NuxtLink>
-      <p class="font-light leading-7 text-gray-500 text-lg">
-        {{ post.title }}
-      </p>
-      <div class="mt-5">
-        <NuxtLink
-          to="#"
-          class="font-medium text-green-600 hover:text-green-700"
-        >
-          Details ->
-        </NuxtLink>
-      </div>
-    </div>
-  </article>
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+      {{ post.title }}
+    </p>
+    <a
+      href="#"
+      class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Read more
+      <svg
+        class="ml-2 -mr-1 w-4 h-4"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
+    </a>
+  </div>
 </template>
 
 <script>
