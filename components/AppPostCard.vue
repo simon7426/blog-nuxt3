07aceaa776a89595ee08d80810442f84db3efa05
simon.islam <simon.islam@brilliant.com.bd>
2022-05-26 17:33:43 +0600

    added author profile and comment section.

--- components/AppPostCard.vue
+++ components/AppPostCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container w-full md:max-w-3xl mx-auto pt-20">
+  <div class="container w-full md:max-w-3xl mx-auto pt-5">
     <div
       class="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal"
       style="font-family: Georgia, serif"
@@ -7,7 +7,7 @@
       <div class="font-sans">
         <NuxtLink :to="post.id">
           <h1
-            class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl"
+            class="font-bold font-sans break-normal text-gray-900 pt-3 pb-2 text-3xl md:text-4xl"
           >
             {{ post.title }}
           </h1>
---
+++ components/CommentListCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3 class="mb-4 text-lg font-semibold text-gray-900">Comments</h3>
+
+    <CommentCard v-for="comment in comments" :key="comment.id" :comment="comment" />
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {
+        comments: {
+            type: Array,
+            required: true,
+            default: () => { },
+        },
+    }
+}
+</script>
